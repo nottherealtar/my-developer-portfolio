557bd532c2da6b733098623179cfe510f345b577
Josh Coetzer <67793100+nottherealtar@users.noreply.github.com>
2025-03-29 14:06:48 +0200

    fixing window pre-rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import dynamic from "next/dynamic";
+import React from "react";
+
 const Navigation = dynamic(() => import("../components/Navigation"));
 const Greetings = dynamic(() => import("../containers/Greetings"));
 const Skills = dynamic(() => import("../containers/Skills"));
@@ -8,6 +10,7 @@ const Experience = dynamic(() => import("../containers/Experience"));
 const Projects = dynamic(() => import("../containers/Projects"));
 const Feedbacks = dynamic(() => import("../containers/Feedbacks"));
 const GithubProfileCard = dynamic(() => import("../components/GithubProfileCard"));
+const SkillCard = dynamic(() => import("../components/SkillCard"), { ssr: false });
 import { openSource } from "../portfolio";
 import SEO from "../components/SEO";
 import { GithubUserType } from "../types";
@@ -24,14 +27,26 @@ export default function Home({ githubProfileData }: { githubProfileData: any }) 
       <Experience />
       <Feedbacks />
       <Projects />
+      <SkillCard
+        title="Full Stack Development"
+        lottieAnimationFile="/lottie/skills/fullstack.json"
+        skills={[
+          "Building scalable and responsive azure solutions using JavaScript",
+          "Building scalable and responsive web applications using Flask",
+          "Building tightly integrated solutions for business applications",
+        ]}
+        softwareSkills={[
+          { skillName: "Python", iconifyTag: "logos:python" },
+          { skillName: "JavaScript", iconifyTag: "logos:javascript" },
+          { skillName: "Heroku", iconifyTag: "logos:heroku-icon" },
+          { skillName: "Github", iconifyTag: "akar-icons:github-fill" },
+          { skillName: "Docker", iconifyTag: "logos:docker-icon" },
+        ]}
+      />
       <GithubProfileCard {...githubProfileData} />
     </div>
   );
 }
-
-// Home.prototype = {
-//   githubProfileData: PropTypes.object.isRequired,
-// };
 
 export async function getStaticProps() {
   const githubProfileData: GithubUserType = await fetch(
